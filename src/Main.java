76af7255d4824e0d579faec9ac47253ebe4cf4bd
droman-tw <droman@thoughtworks.com>
2016-05-25 17:21:31 -0500

    Adding Triangle Done
    Adding the code for the Triangle classes

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by droman on 5/25/16.
+ */
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(Triangle.drawOneStar()+"\n");
+
+        System.out.println(Triangle.drawHorizontalLine(4)+"\n");
+
+        System.out.println(Triangle.drawVerticalLine(2)+"\n");
+
+        System.out.println(Triangle.drawRightTriangle(5));
+    }
+}
--- src/Triangle.java
+++ src/Triangle.java
@@ -2,4 +2,42 @@
  * Created by droman on 5/25/16.
  */
 public class Triangle {
+
+    //Method to draw one star
+    public static String drawOneStar(){
+        return "*";
+    }
+
+    //Method to draw a lines of stars given a constant
+    public static String drawHorizontalLine(int n){
+        String line = "";
+
+        for(int i=0; i<n; i++){
+            line += drawOneStar();
+        }
+        return line;
+    }
+
+    public static String drawVerticalLine(int n){
+        String line = "";
+
+        for(int i=0; i<n; i++){
+            line += drawOneStar() + "\n";
+        }
+
+        return line;
+    }
+
+    public static String drawRightTriangle(int n){
+        String triangle="";
+
+        for(int i=1; i <= n; i++){
+            triangle += drawHorizontalLine(i) + "\n";
+        }
+
+        return triangle;
+    }
+
+
+
 }
