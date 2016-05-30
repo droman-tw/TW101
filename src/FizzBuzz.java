5d60b40c46538f64a50ac095edf8b7b41aad870a
droman-tw <droman@thoughtworks.com>
2016-05-30 10:22:48 -0500

    Refactor of FizzBuzz
    Refactor the FizzBuzz game by encapsulating in a method the part of the
    code that identifies if a number is a regular one or FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,21 +3,29 @@
  */
 public class FizzBuzz {
 
+    //This method determines if a number would be Fizz and/or Buzz, or a
+    //regular number
+    public static String categorize(int n) {
+        String number ="";
+
+        if(n%5 != 0 && n%3 != 0){
+            number += n;
+        }
+        else{
+            if(n%5 == 0){
+                number+= "Fizz";
+            }
+            if(n%3 == 0){
+                number+="Buzz";
+            }
+        }
+        return number;
+    }
+
     public static String playGame(){
         String output = "";
         for(int i=1; i<=100; i++){
-            if(i%5 != 0 && i%3 != 0){
-                output += i;
-            }
-            else{
-                if(i%5 == 0){
-                   output+= "Fizz";
-                }
-                if(i%3 == 0){
-                    output+="Buzz";
-                }
-            }
-
+            output += categorize(i);
             output += "\n";
         }
         return output;
