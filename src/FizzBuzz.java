6d5f50e15d4abff1f412a09f41cd5116ba82a965
droman-tw <droman@thoughtworks.com>
2016-05-31 10:52:53 -0500

    Second Last Commmit
    Commit with all the comments

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,6 +5,12 @@ public class FizzBuzz {
 
     //This method determines if a number would be Fizz and/or Buzz, or a
     //regular number
+
+    /**
+     * This method indentifies if a number is a regular one, Fizz and/or Buzz
+     * @param n This is the number to be identified
+     * @return String returns a number with its FizzBuzz Machine indentifier
+     */
     public static String categorize(int n) {
         String number ="";
 
@@ -12,15 +18,21 @@ public class FizzBuzz {
             number += n;
         }
         else{
-            if(n%5 == 0){
+            //Check if it is divisible By
+            if(n%3 == 0){
                 number+= "Fizz";
             }
-            if(n%3 == 0){
+            if(n%5 == 0){
                 number+="Buzz";
             }
         }
         return number;
     }
+
+    /**
+     * This method runs the FizzBuzz game with the numbers 1 through 100
+     * @return String This returns the results of the game.
+     */
 
     public static String playGame(){
         String output = "";
--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,36 @@
 
 public class Main {
     public static void main(String[] args){
+
+        //Triangle: Exercise 1
         System.out.println(Triangle.drawOneStar()+"\n");
 
+        //Triangle: Exercise 2
         System.out.println(Triangle.drawHorizontalLine(4)+"\n");
 
+        //Triangle: Exercise 3
         System.out.println(Triangle.drawVerticalLine(2)+"\n");
 
+        //Triangle: Exercise 4
         System.out.println(Triangle.drawRightTriangle(5));
 
+        //Diamond: Exercise 1
         System.out.println(Diamond.drawIsosceles(5));
 
+        //Diamond: Exercise 2
         System.out.println(Diamond.drawDiamond(5));
 
+        //Diamond: Exercise 3
         System.out.println(Diamond.drawDiamondName(3,"Billy"));
 
+        //FizzBuzz Exercise
         System.out.println(FizzBuzz.playGame());
 
-        System.out.println(PrimeFactors.primeFactorization(89));
+        //Prime Factors: Different cases
+        System.out.println(PrimeFactors.generate(1));//Base case
+        System.out.println(PrimeFactors.generate(89));//Prime number case
+        System.out.println(PrimeFactors.generate(10));
+        System.out.println(PrimeFactors.generate(49));
 
     }
 }
